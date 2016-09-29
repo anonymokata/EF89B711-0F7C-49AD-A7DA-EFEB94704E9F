97fced8f418c9ec5e6e2663b9db677c1b59142ce
anonymous <anonymous>
2016-09-29 08:13:58 -0500

    deleted two test cases that did not coincide with the red green refactor TDD model

--- RomanNum_TEST.c
+++ RomanNum_TEST.c
@@ -31,19 +31,9 @@ START_TEST(WhenMultiRomanNumIsPassedTheyAreReturned)
 }
 END_TEST
 
-START_TEST(TestForCorrectInputFormat)
-{
-#line 17
-	//match = strcmp(RomanNum("MMMCMXCIXII"),"MMMCMXCIXII");
-	//fail_unless(match != 0, "Testing for correct formatting, MMMCMXCIXII is not a valid input due to the two 'I's after the X");
-	fail_unless(1,"Phase one complete");
-
-}
-END_TEST
-
 START_TEST(FirstAddOfOnePlusOne)
 {
-#line 22
+#line 17
 	match = strcmp(RomanNum("I","I"), "II");
 	fail_unless(match == 0, "Simple add of one plus one");	
 
@@ -52,7 +42,7 @@ END_TEST
 
 START_TEST(UnpackAndConcatenateRomanNum)
 {
-#line 26
+#line 21
 	//match = strcmp(RomanNum("CCCLXIX","DCCCXLV"), "CCCXXXXXXIIIIIIIIICCCCCCCCXXXXIIIII");
 	//fail_unless(match == 0, "1st step in adding two roman num's together is to concatenate and then unpack the concatenated number into I X C and M");
 	fail_unless(1,"omitting test for 1st step of addition"); 
@@ -62,7 +52,7 @@ END_TEST
 
 START_TEST(SortTheUnpackedRomanNum)
 {
-#line 31
+#line 26
 	//match = strcmp(RomanNum("CCCLXIX","DCCCXLV"), "CCCCCCCCCCCXXXXXXXXXXIIIIIIIIIIIIII");
 	//fail_unless(match == 0,"2nd step in adding two roman num's is to sort the unpacked num");
 	fail_unless(1,"omitting test for 2nd step of addition"); 
@@ -72,7 +62,7 @@ END_TEST
 
 START_TEST(CompressTheSortedRomanNum)
 {
-#line 36
+#line 31
 	match = strcmp(RomanNum("CCCLXIX","DCCCXLV"), "MCCXIV");
 	fail_unless(match == 0,"3rd and final step of addition, to compress the sorted Roman Num");
 
@@ -81,18 +71,10 @@ END_TEST
 
 START_TEST(Subtraction)
 {
-#line 40
+#line 35
 	match = strcmp(RomanNum("XLIII","CXXIX"), "LXXXVI");
 	fail_unless(match == 0,"Subtraction, to Subtraction smaller numeral must be in front of a larger one.");
 
-}
-END_TEST
-
-START_TEST(TestingIfProgWillExitIfOver3999)
-{
-#line 44
-	match = strcmp(RomanNum("MM","MM"), "MMMM");
-	fail_unless(match != 0,"Testing if program will give an error if given a number over 3999");
 }
 END_TEST
 
@@ -106,13 +88,11 @@ int main(void)
     suite_add_tcase(s1, tc1_1);
     tcase_add_test(tc1_1, WhenSingleRomanNumIsPassedACharReturnThatChar);
     tcase_add_test(tc1_1, WhenMultiRomanNumIsPassedTheyAreReturned);
-    tcase_add_test(tc1_1, TestForCorrectInputFormat);
     tcase_add_test(tc1_1, FirstAddOfOnePlusOne);
     tcase_add_test(tc1_1, UnpackAndConcatenateRomanNum);
     tcase_add_test(tc1_1, SortTheUnpackedRomanNum);
     tcase_add_test(tc1_1, CompressTheSortedRomanNum);
     tcase_add_test(tc1_1, Subtraction);
-    tcase_add_test(tc1_1, TestingIfProgWillExitIfOver3999);
 
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
